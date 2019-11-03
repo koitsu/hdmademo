b208d371355e9bb84bd4a862a58f74ad3e44ac21
Jeremy Chadwick <jdc@koitsu.org>
2019-11-03 01:42:56 -0700

    ca65: don't use --dbgfile, it sometimes can confuse Mesen

--- ca65/make.bat
+++ ca65/make.bat
@@ -2,7 +2,7 @@
 set name=hdmademo
 
 set ca65flags=-g
-set ld65flags=-m %name%.map -Ln %name%.lbl --dbgfile %name%.dbg
+set ld65flags=-m %name%.map -Ln %name%.lbl
 
 ca65 %ca65flags% -l %name%.lst -o %name%.o %name%.asm
 ld65 %ld65flags% -C ld65.cfg -o %name%.sfc %name%.o
